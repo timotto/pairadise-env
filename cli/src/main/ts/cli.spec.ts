42c8846a764676f7303ba6d749d3c10733fe7e1d
Tim Otto <tim@timotto.io>
2018-03-03 19:30:15 +0100

    chore: implement node js argument parsing

--- cli/src/main/ts/cli.spec.ts
+++ cli/src/main/ts/cli.spec.ts
@@ -1,7 +1,15 @@
 import {Cli} from './cli';
 
 describe('Cli', () => {
-    it('works', async () => {
-        await Cli.main([]);
-    })
+    describe('main(args)', () => {
+        it('requires at least two arguments', async () => {
+            await Cli.main([])
+                .then(() => fail())
+                .catch(e => expect(e).toBeDefined());
+            await Cli.main(['one'])
+                .then(() => fail())
+                .catch(e => expect(e).toBeDefined());
+            await Cli.main(['one', 'two']);
+        });
+    });
 });
